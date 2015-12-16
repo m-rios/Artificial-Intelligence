b9a00b06788fe7a4dd06ec003196cce10a1c5563
Mario Rios <mrm994@gmail.com>
2015-12-16 15:15:37 +0100

    k_means algorithm implemented but not working, must be debugged

---
+++ Lab 3/main.py
@@ -0,0 +1,102 @@
+import random
+import copy
+
+def parse_wine(path):
+    f = open(path, 'r')
+    data_set = []
+    size = 178
+
+    for line in f.readlines():
+        row = line.split(',')
+        # row[len(row)-1] = row[len(row)-1].rstrip()
+        num_row = []
+        for i in range(len(row)):
+            num_row.append(float(row[i]))
+        data_set.append(row)
+    f.close()
+    return data_set
+
+
+def rand_init_cluster(data_set, n_clusters):
+    c = {}
+    for i in range(n_clusters):
+        c[i] = []
+
+    for line in data_set:
+        c[random.randrange(0,n_clusters)].append(line)
+
+    return c
+
+
+def mean(cluster):
+    mean = copy.copy(cluster[0])
+
+    for i in range(1, len(cluster)):
+        for j in range(len(cluster[i])):
+            mean[j] = mean[j] = cluster[i][j]
+    mean.pop(0)
+    result = [x/len(cluster) for x in mean]
+
+    return result
+
+
+def distance(wine, cluster):
+    result = 0
+    for i in range(len(cluster)):
+        result += abs(wine[i+1] - cluster[i])
+    return result
+
+
+def k_means_clustering(data_set, n_clusters):
+
+    c_new = rand_init_cluster(data_set, n_clusters)
+    #new dict
+    c = {}
+    for i in range(n_clusters):
+        c_new[i] = []
+
+    while not c == c_new:
+
+        c = dict(c_new)
+        # clear c_new
+        for key in c_new.keys():
+            c_new[key] = []
+
+        #get mean / cluster
+        means = []
+        for i in range(n_clusters):
+            means[i] = mean(c[i])
+
+        #Compute distance map
+        distance_array = []
+        for i in range(n_clusters):
+            distance_row = []
+            for j in range(len(data_set)):
+                distance_row.append(distance(data_set[j], means[i]))
+            distance_array.append(distance_row)
+
+        #Re-arrange
+        for i in range(data_set):
+            column = []
+            for j in range(n_clusters):
+                column.append(distance_array[j][i])
+            min = min(column)
+            index = column.index(min)
+            c_new[index].append(data_set[i])
+
+    return c
+
+if __name__ == '__main__':
+    data = parse_wine("wine.data")
+
+    # for i in range(len(data)):
+    #     line = ""
+    #     for j in range(len(data[0])):
+    #         line += data[i][j]+","
+    #     print line
+
+
+    c = k_means_clustering(data,3)
+    print c[0]
+    print c[1]
+    print c[2]
